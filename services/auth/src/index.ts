f576bc9e7de1acadd0f46b3e43b5efb984e859a2
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-04-27 11:05:09 +0530

    creating routing

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import AuthRoutes from "./routes/auth.js"
 import connectDB from "./config/db.js";
 dotenv.config();
 
-const app=express();
-
+const app = express();
+connectDB();
 const PORT = process.env.PORT || 3000;
+app.use("/api/auth", AuthRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
